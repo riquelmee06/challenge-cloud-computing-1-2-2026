53234cefa6948705781d12739a26c19a7148cb44
Riquelme Nascimento <riquelmenacimento40@gmail.com>
2026-05-24 07:14:02 -0300

    Update VM creation script

--- criar-vm.sh
+++ criar-vm.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
-
 RESOURCE_GROUP="rg-challenge-565468"
 LOCATION="eastus2"
 VM_NAME="vmdocker565468"
 IMAGE="dockerinc1694120899427:devbox_azuremachine:devboxlicensefpromo:4.41.2"
-
 SIZE="Standard_D2s_v3"
 ADMIN_USERNAME="rm565468"
 ADMIN_PASSWORD="Fiap@2tdsvms"
-
+DISK_SKU="StandardSSD_LRS"
 PORT=3389
 SHUTDOWN_TIME="0230" 
-
 echo "Criando grupo de recursos: $RESOURCE_GROUP..."
 az group create --name $RESOURCE_GROUP --location $LOCATION
-
 echo "Aceitando os Termos Legais da Imagem..."
 az vm image terms accept --urn $IMAGE
-
 echo "Criando a máquina virtual: $VM_NAME..."
 az vm create \
-  --resource-group $RESOURCE_GROUP \
-  --name $VM_NAME \
-  --image $IMAGE \
-  --size $SIZE \
-  --authentication-type password \
-  --admin-username $ADMIN_USERNAME \
-  --admin-password $ADMIN_PASSWORD \
-  --public-ip-sku Standard
-
+  --resource-group $RESOURCE_GROUP \
+  --name $VM_NAME \
+  --image $IMAGE \
+  --size $SIZE \
+  --authentication-type password \
+  --admin-username $ADMIN_USERNAME \
+  --admin-password $ADMIN_PASSWORD \
+  --storage-sku $DISK_SKU \
+  --public-ip-sku Standard
 echo "Abrindo portas para RDP, API (8080) e Banco Oracle (1521)..."
 az vm open-port --port $PORT --resource-group $RESOURCE_GROUP --name $VM_NAME --priority 1003
 az vm open-port --port 8080 --resource-group $RESOURCE_GROUP --name $VM_NAME --priority 1004
 az vm open-port --port 1521 --resource-group $RESOURCE_GROUP --name $VM_NAME --priority 1005
-
 echo "Configurando desligamento automático às $SHUTDOWN_TIME (UTC)..."
 az vm auto-shutdown \
-  --resource-group $RESOURCE_GROUP \
-  --name $VM_NAME \
-  --time $SHUTDOWN_TIME
-
+  --resource-group $RESOURCE_GROUP \
+  --name $VM_NAME \
+  --time $SHUTDOWN_TIME
 echo "Provisionamento completo!"
